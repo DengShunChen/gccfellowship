682dafbb1de9d3d4afeb50959b40d8e8a874e600
DengShunChen <dschen@cwb.gov.tw>
2019-06-20 14:58:59 +0800

    add audio handing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python 
 from datetime import datetime, timedelta
 from flask import Flask, request, abort
 import random
@@ -85,11 +86,40 @@ def handle_sticker_message(event):
     )
 #   line_bot_api.reply_message(event.reply_token,sticker_message)
 
+# 處理內部傳送訊息
 @handler.add(PostbackEvent)
 def handle_postback(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     user_name = profile.display_name
     data = event.postback.data
+
+# 處理音訊訊息
+from pydub import AudioSegment
+import speech_recognition as sr
+@handler.add(MessageEvent,message=AudioMessage)
+def handle_aud(event):
+    r = sr.Recognizer()
+    message_content = line_bot_api.get_message_content(event.message.id)
+    ext = 'mp3'
+    try:
+        with tempfile.NamedTemporaryFile(prefix=ext + '-', delete=False) as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+            tempfile_path = tf.name
+        path = tempfile_path
+        AudioSegment.converter = '/app/vendor/ffmpeg/ffmpeg'
+        sound = AudioSegment.from_file_using_temporary_files(path)
+        path = os.path.splitext(path)[0]+'.wav'
+        sound.export(path, format="wav")
+        with sr.AudioFile(path) as source:
+            audio = r.record(source)
+    except Exception as e:
+        t = '音訊有問題'+test+str(e.args)+path
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=t))
+    os.remove(path)
+    text = r.recognize_google(audio,language='zh-TW')
+    message = audio_template(text)
+    line_bot_api.reply_message(event.reply_token,message)
 
 import os
 if __name__ == "__main__":
