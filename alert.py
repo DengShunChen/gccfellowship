93197b4f8aec0547b4360c600ab2561c37f4b65a
DengShunChen <dschen@cwb.gov.tw>
2019-06-20 11:54:18 +0800

    fix if only have single message of alert

--- alert.py
+++ alert.py
@@ -15,22 +15,36 @@ def show_alert(data):
   authorlist = ['中央氣象局']
   keywordlist=['桃園市','台北市','新北市','新竹市','新竹縣']
   count = 1
-  for message in data:
-    print('message :',message)
-    author = message['author']['name']
-    summary = message['summary']['#text'].strip()
+  if type(data) is dict:
+    author = data['author']['name']
+    summary = data['summary']['#text'].strip()
 
     stringfind = False
     for keyword in keywordlist:
       if summary.find(keyword) != -1:
         stringfind = True
         break
-
     authorfind = author in authorlist
-
     if authorfind or stringfind :
       string = string + '%2d. %s: \n%s \n\n' % (count,author,summary)
       count+=1
+  else: 
+    for message in data:
+      print('message :',message)
+      author = message['author']['name']
+      summary = message['summary']['#text'].strip()
+
+      stringfind = False
+      for keyword in keywordlist:
+        if summary.find(keyword) != -1:
+          stringfind = True
+          break
+      authorfind = author in authorlist
+      if authorfind or stringfind :
+        string = string + '%2d. %s: \n%s \n\n' % (count,author,summary)
+        count+=1
+  if count == 1:
+     string = string + '您好！目前沒有警特報訊息！'
   return string
 
 def show_all(data):
